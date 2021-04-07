7d3e4690c5947210e2bad09453b571412ab065f1
ZivkovicMil <75956995+ZivkovicMil@users.noreply.github.com>
2021-04-07 14:31:32 +0200

    test

--- src/main/java/com/webShop/webShop/entities/User.java
+++ src/main/java/com/webShop/webShop/entities/User.java
@@ -52,6 +52,8 @@ public class User {
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private ConfirmationToken confirmationToken;
 
+
+
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private ChangePasswordToken changePasswordToken;
 
